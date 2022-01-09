92843517cdde0888774a9a0963d40db03ec17105
Gabe R <rodriguezgab1601@gmail.com>
2022-01-09 21:19:55 +0800

    Add comments

--- mirai.py
+++ mirai.py
@@ -1,17 +1,22 @@
 import discord;
 import json;
 
-bot = discord.Bot();
+mirai = discord.Bot();
 
+# Reads config json file
 with open("config/mirai.json") as file:
   config = json.load(file);
   
-@bot.event
+# Event handlers  
+@mirai.event
+# Function that fires when mirai boots up and successfully logs in
 async def on_ready():
-    dev_channel = bot.get_channel(929427727033982986);
-    print(f"We have logged in as {bot.user}");
+    dev_channel = mirai.get_channel(929427727033982986);
+    print(f"We have logged in as {mirai.user}");
     await dev_channel.send("I'm booting up! (◕ᴗ◕✿)");
 
-bot.load_extension('cogs.about');
+# Loads cogs into the mirai
+# TODO: Make it a loop of the whole cogs folder
+mirai.load_extension('cogs.about');
 
-bot.run(config["token"]);
+mirai.run(config["token"]);
