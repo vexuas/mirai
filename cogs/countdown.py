2b2bce20af3cd14b95bcd3532827630fea5ec78e
Gabe R <rodriguezgab1601@gmail.com>
2022-01-10 23:59:58 +0800

    Add options to commands

--- cogs/countdown.py
+++ cogs/countdown.py
@@ -1,5 +1,4 @@
-from re import L
-from discord.commands import slash_command;
+from discord.commands import slash_command, Option;
 from discord.ext import commands;
 
 class Countdown(commands.Cog):
@@ -7,8 +6,12 @@ class Countdown(commands.Cog):
     self.bot = bot;
 
   @slash_command(guild_ids=[929426428003483720], description="Starts a countdown from a set number of days")
-  async def countdown(self, ctx):
-    await ctx.respond('Countdown command');
+  async def countdown(self, ctx, days: Option(int, "Enter number of days!", required=False)):
+    if not days:
+      return await ctx.respond('Countdown information!');
+      
+    return await ctx.respond(f'Coundown with {days} days!');
+      
 
 def setup(bot: commands.Bot):
   bot.add_cog(Countdown(bot));
