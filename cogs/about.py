3ebaae360fe947f6b3e020e7b3a50681c16eebd0
Gabriel R <42207245+vexuas@users.noreply.github.com>
2022-01-11 23:00:16 +0800

    Add countdown UI (#6)
    * Create countdown info embed helper
    
    * Create buttons
    
    * Move helpers to respective classes
    
    * Create generate button function
    
    * Create generate confirmation embed
    
    * Add comments

--- cogs/about.py
+++ cogs/about.py
@@ -1,15 +1,40 @@
+import json;
 from discord.commands import slash_command;
 from discord.ext import commands;
-from helpers import generate_about_embed;
+from helpers import generate_embed;
+
+# Reads package.json file; mostly to retrieve bot version
+with open("package.json") as file:
+  package = json.load(file);
+  
+with open("config/mirai.json") as file:
+  config = json.load(file);
 
 class About(commands.Cog):
   def __init__(self, bot: commands.Bot):
     self.bot = bot;
 
-  @slash_command(guild_ids=[929426428003483720], description="Displays information about Mirai")
+  # Generates the embed going to be sent for the about command
+  # client: initialising discord client
+  def generate_about_embed(self, client):
+    embed = generate_embed();
+    embed.title = "About";
+    embed.description = "Hi there! I'm Mirai and I'm a simple bot that counts down from a set number of days!\n\nMy creator made me as it helps him stay focused and keep him in check with his personal goals. Also because he wanted to create his first Python Project!\n\nI hope I can help you as much as I've helped him! (◕ᴗ◕✿)";
+
+    embed.add_field(name="Creator", value="Vexuas#8141", inline=True);
+    embed.add_field(name="Date Created", value=client.user.created_at.strftime('%d-%b-%Y'), inline=True);
+    embed.add_field(name="Version", value=package["version"], inline=True);
+    embed.add_field(name="Library", value="Pycord", inline=True);
+    embed.add_field(name="Last Update", value="10-Jan-2022", inline=True);
+    embed.add_field(name="Source Code", value="[Github](https://github.com/vexuas/mirai)", inline=True);
+
+    return embed;
+
+  # Registering slash command
+  @slash_command(guild_ids=config["guildIDs"], description="Displays information about Mirai")
   async def about(self, ctx):
-    embed = generate_about_embed(self.bot);
-    await ctx.respond(embed=embed);
+    embed = self.generate_about_embed(ctx);
+    return await ctx.respond(embed=embed);
   
 
 def setup(bot: commands.Bot):
--- cogs/countdown.py
+++ cogs/countdown.py
@@ -1,17 +1,63 @@
+import discord;
+import json;
 from discord.commands import slash_command, Option;
-from discord.ext import commands;
+from discord.ext import commands
+from discord.ui import Button, View
 
+from helpers import generate_embed;
+
+with open("config/mirai.json") as file:
+  config = json.load(file);
+  
 class Countdown(commands.Cog):
   def __init__(self, bot: commands.Bot):
     self.bot = bot;
+      
+  # Generates embed going to be sent for the default countdown command    
+  def generate_countdown_information_embed(self):
+    embed = generate_embed();
+    embed.title = "Countdown | Help";
+    embed.description = "The Countdown command, as the name suggests, counts down from a specified number of days.\n\nWhen prompted, I will create a separate channel to keep track of the remaining days and will ping you there daily until the end!\n\nTo use this command, simply add your desired days after `/countdown`";
 
-  @slash_command(guild_ids=[929426428003483720], description="Starts a countdown from a set number of days")
+    return embed;     
+
+  # Generates embed going to be sent when confirming the countdown command
+  # This is shown when the user uses the countdown command with the days argument
+  # days: number of days - int
+  def generate_countdown_confirmation_embed(self, days):
+    embed = generate_embed();
+    embed.title = "Countdown | Confirmation";
+    embed.description = f"You've created a countdown of {days} days.\n\nBy confirming below, I'll create a separate channel where I'll ping you daily on the number of days left\n\nGood luck with your goal! :D"
+
+    return embed;
+
+  # Generates the buttons view
+  # Components need to be inside a View for them to properly work; View can be taken as a container
+  # Buttons are inserted and pushed to the right with subsequent additions
+  # A tad bit frustrating as there's currently no option to make them float right; Messages look off with buttons and a long length embed
+  # Maybe check if components can be inserted inside an embed
+  def generate_buttons(self):
+    confirm_button = Button(label="Let's go!", style=discord.ButtonStyle.success);
+    cancel_button = Button(label="Cancel", style=discord.ButtonStyle.secondary);
+
+    view = View();
+    view.add_item(cancel_button);
+    view.add_item(confirm_button);
+
+    return view;
+    
+  # Registering slash command
+  @slash_command(guild_ids=config["guildIDs"], description="Starts a countdown from a set number of days")
   async def countdown(self, ctx, days: Option(int, "Enter number of days!", required=False)):
     if not days:
-      return await ctx.respond('Countdown information!');
-      
-    return await ctx.respond(f'Coundown with {days} days!');
-      
+      embed = self.generate_countdown_information_embed();
+      return await ctx.respond(embed=embed);
+
+    actions_view = self.generate_buttons();
+    embed = self.generate_countdown_confirmation_embed(days);
+
+    return await ctx.respond(embed=embed, view=actions_view);
+
 
 def setup(bot: commands.Bot):
   bot.add_cog(Countdown(bot));
