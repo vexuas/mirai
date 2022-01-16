326aaad2bad3c746552eaf788a0d0be6458c5817
Gabriel R <42207245+vexuas@users.noreply.github.com>
2022-01-17 00:56:47 +0800

    Release/2022 01 17 (#15)
    * Update CHANGELOG.md [skip ci]
    
    * v1.0.0
    
    * Update date
    
    * Fix about created at

--- cogs/about.py
+++ cogs/about.py
@@ -17,16 +17,16 @@ class About(commands.Cog):
 
   # Generates the embed going to be sent for the about command
   # client: initialising discord client
-  def generate_about_embed(self, client):
+  def generate_about_embed(self):
     embed = Helpers().generate_embed();
     embed.title = "About";
     embed.description = "Hi there! I'm Mirai and I'm a simple bot that counts down from a set number of days!\n\nMy creator made me as it helps him stay focused and keep him in check with his personal goals. Also because he wanted to create his first Python Project!\n\nI hope I can help you as much as I've helped him! (◕ᴗ◕✿)";
 
     embed.add_field(name="Creator", value="Vexuas#8141", inline=True);
-    embed.add_field(name="Date Created", value=client.user.created_at.strftime('%d-%b-%Y'), inline=True);
+    embed.add_field(name="Date Created", value=self.bot.user.created_at.strftime('%d-%b-%Y'), inline=True);
     embed.add_field(name="Version", value=package["version"], inline=True);
     embed.add_field(name="Library", value="Pycord", inline=True);
-    embed.add_field(name="Last Update", value="10-Jan-2022", inline=True);
+    embed.add_field(name="Last Update", value="17-Jan-2022", inline=True);
     embed.add_field(name="Source Code", value="[Github](https://github.com/vexuas/mirai)", inline=True);
 
     return embed;
@@ -34,7 +34,7 @@ class About(commands.Cog):
   # Register slash command and main handler for initialisation
   @slash_command(description="Displays information about Mirai")
   async def about(self, ctx):
-    embed = self.generate_about_embed(ctx);
+    embed = self.generate_about_embed();
     return await ctx.respond(embed=embed);
   
 
